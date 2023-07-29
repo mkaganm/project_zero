2d48ff06958a69210e958de981a5e6bc14bc56d5
kagan.meric <mustafakaganmeric@gmail.com>
2023-07-29 17:05:21 +0300

    update logger service

--- loggerservice/internal/api/sendLog2Elastic.go
+++ loggerservice/internal/api/sendLog2Elastic.go
@@ -2,20 +2,53 @@ package api
 
 import (
 	"encoding/json"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"loggerservice/internal/data/elastic"
+	"loggerservice/internal/utils"
 )
+
+type ElasticRequest struct {
+	Index string      `json:"index" validate:"required"`
+	Data  interface{} `json:"data" validate:"required"`
+}
 
 // SendLog2Elastic sends log to Elasticsearch
 func SendLog2Elastic(c *fiber.Ctx) error {
 
 	request := c.Request()
-	var data interface{}
-	_ = json.Unmarshal(request.Body(), &data)
+	var req ElasticRequest
 
-	go func() {
-		elastic.SendLog2Elastic(data)
-	}()
+	err := json.Unmarshal(request.Body(), &req)
+	if err != nil {
+		utils.LogErr("Error unmarshaling request body:", err)
+		return c.Status(fiber.StatusBadRequest).JSON(ErrorResponse{
+			Status:       "failed",
+			Error:        err,
+			ErrorMessage: err.Error(),
+		})
+	}
+
+	validate := validator.New()
+	err = validate.Struct(req)
+	if err != nil {
+		utils.LogErr("Error validating request:", err)
+		return c.Status(fiber.StatusBadRequest).JSON(ErrorResponse{
+			Status:       "failed",
+			Error:        err,
+			ErrorMessage: err.Error(),
+		})
+	}
+
+	err = elastic.SendLog2Elastic(req.Data, req.Index)
+	if err != nil {
+		utils.LogErr("Error sending log to Elasticsearch:", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(ErrorResponse{
+			Status:       "failed",
+			Error:        err,
+			ErrorMessage: err.Error(),
+		})
+	}
 
 	return c.Status(fiber.StatusOK).JSON(map[string]string{
 		"status": "success",
--- loggerservice/internal/api/sendLog2Mongo.go
+++ loggerservice/internal/api/sendLog2Mongo.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"loggerservice/internal/config"
 	"loggerservice/internal/data/mongo"
@@ -11,10 +12,14 @@ import (
 
 type LogRequest struct {
 	Collection     string      `json:"collection" validate:"required"`
+	Source         string      `json:"source"`
+	Method         string      `json:"method"`
 	Request        interface{} `json:"request"`
-	RequestHeader  interface{} `json:"requestHeader"`
+	RequestHeader  interface{} `json:"request_header"`
 	Response       interface{} `json:"response"`
-	ResponseHeader interface{} `json:"responseHeader"`
+	ResponseHeader interface{} `json:"response_header"`
+	Duration       string      `json:"duration"`
+	Status         int         `json:"status"`
 }
 
 // SendLog2Mongo sends success log to mongo
@@ -22,32 +27,37 @@ func SendLog2Mongo(c *fiber.Ctx) error {
 
 	request := c.Request()
 
-	go func() {
+	var req LogRequest
+	err := json.Unmarshal(request.Body(), &req)
+	utils.LogErr("Error unmarshaling request body:", err)
 
-		var req LogRequest
-		err := json.Unmarshal(request.Body(), &req)
-		utils.LogErr("Error unmarshaling request body:", err)
+	validate := validator.New()
+	err = validate.Struct(req)
+	utils.LogErr("Error validating request body:", err)
 
-		client := mongo.InitDB()
-		defer mongo.CloseDB(client)
+	client := mongo.InitDB()
+	defer mongo.CloseDB(client)
 
-		db := client.Database(config.EnvConfigs.MongoDb)
-		collection := db.Collection(req.Collection)
+	db := client.Database(config.EnvConfigs.MongoDb)
+	collection := db.Collection(req.Collection)
 
-		LogMongo := mongo.LogMongo{
-			Request:        req.Request,
-			RequestHeader:  req.RequestHeader,
-			Response:       req.Response,
-			ResponseHeader: req.ResponseHeader,
-			Timestamp:      time.Now(),
-		}
+	LogMongo := mongo.LogMongo{
+		Source:         req.Source,
+		Method:         req.Method,
+		Request:        req.Request,
+		RequestHeader:  req.RequestHeader,
+		Response:       req.Response,
+		ResponseHeader: req.ResponseHeader,
+		Duration:       req.Duration,
+		Status:         req.Status,
+		Timestamp:      time.Now(),
+	}
 
-		_, err = collection.InsertOne(c.Context(), LogMongo)
-		utils.LogErr("Error inserting log to MongoDB:", err)
+	result, err := collection.InsertOne(c.Context(), LogMongo)
+	utils.LogErr("Error inserting log to mongo:", err)
 
-	}()
-
-	return c.Status(fiber.StatusOK).JSON(map[string]string{
+	return c.Status(fiber.StatusOK).JSON(map[string]interface{}{
 		"status": "success",
+		"_id":    result.InsertedID,
 	})
 }
