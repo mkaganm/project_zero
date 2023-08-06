3d4bfae2a4e380ef28238780ba07f3e6480b5463
kagan.meric <mustafakaganmeric@gmail.com>
2023-08-07 02:34:51 +0300

    update loggerservice

---
+++ loggerservice/elastic.cmd
@@ -0,0 +1,50 @@
+curl -XPUT "http://localhost:9200/userservice" -H 'Content-Type: application/json' -d '{
+  "settings": {
+    "number_of_shards": 1,
+    "number_of_replicas": 1
+  },
+  "mappings": {
+    "properties": {
+      "timestamp": {
+        "type": "date"
+      },
+      "data": {
+        "type": "object"
+      }
+    }
+  }
+}'
+
+curl -XPUT "http://localhost:9200/loggerservice" -H 'Content-Type: application/json' -d '{
+  "settings": {
+    "number_of_shards": 1,
+    "number_of_replicas": 1
+  },
+  "mappings": {
+    "properties": {
+      "timestamp": {
+        "type": "date"
+      },
+      "data": {
+        "type": "object"
+      }
+    }
+  }
+}'
+
+curl -XPUT "http://localhost:9200/cronitor" -H 'Content-Type: application/json' -d '{
+  "settings": {
+    "number_of_shards": 1,
+    "number_of_replicas": 1
+  },
+  "mappings": {
+    "properties": {
+      "timestamp": {
+        "type": "date"
+      },
+      "data": {
+        "type": "object"
+      }
+    }
+  }
+}'
