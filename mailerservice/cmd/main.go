ab1a1ed5c5af6a1096ac0dc627e6166756cda6d9
kagan.meric <mustafakaganmeric@gmail.com>
2023-08-09 01:48:28 +0300

    update mailer

--- mailerservice/cmd/main.go
+++ mailerservice/cmd/main.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"mailerservice/internal/api"
-	"mailerservice/internal/config"
-	"mailerservice/internal/utils"
+	"mailerservice/internal/consumer"
 )
 
 func main() {
 
-	app := fiber.New(fiber.Config{
-		//ReadTimeout:   time.Second * 15,
-		//WriteTimeout:  time.Second * 15,
-		Concurrency:  10,
-		ServerHeader: "user_service_v1",
-		AppName:      "user_service_v1",
-	})
-
-	// Register routes
-	api.RegisterRoutes(app)
-
-	// Listen on port
-	err := app.Listen(config.EnvConfigs.LocalServerPort)
-	utils.FatalErr("Error while serving the api", err)
+	// Consume mailer_queue
+	consumer.ConsumeMailerQueue()
 }
