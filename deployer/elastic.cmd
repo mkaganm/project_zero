ed73d1ae0dde6ab89cc084b9558477270c301256
kagan.meric <mustafakaganmeric@gmail.com>
2023-08-11 09:28:02 +0300

    update

--- deployer/elastic.cmd
+++ deployer/elastic.cmd
@@ -32,6 +32,23 @@ curl -XPUT "http://localhost:9200/loggerservice" -H 'Content-Type: application/j
   }
 }'
 
+curl -XPUT "http://localhost:9200/mailerservice" -H 'Content-Type: application/json' -d '{
+  "settings": {
+    "number_of_shards": 1,
+    "number_of_replicas": 1
+  },
+  "mappings": {
+    "properties": {
+      "timestamp": {
+        "type": "date"
+      },
+      "data": {
+        "type": "object"
+      }
+    }
+  }
+}'
+
 curl -XPUT "http://localhost:9200/cronitor" -H 'Content-Type: application/json' -d '{
   "settings": {
     "number_of_shards": 1,
